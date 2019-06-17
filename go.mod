689592b7c09c659e742189838f2b7724be9a98ee
openset <openset.wang@gmail.com>
2019-06-17 20:33:39 +0800

    Add: mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/openset/hosts
+
+go 1.12
--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
-	"os"
-	"os/exec"
 	"sort"
 )
 
@@ -19,7 +15,6 @@ func main() {
 		"golang.org",
 	}
 	sort.Strings(hosts)
-	var buf bytes.Buffer
 	for _, host := range hosts {
 		addrs, err := net.LookupHost(host)
 		if err != nil {
@@ -27,30 +22,8 @@ func main() {
 		}
 		for _, addr := range addrs {
 			if len(addr) < 16 {
-				row := fmt.Sprintf("%-16s%s\n", addr, host)
-				fmt.Print(row)
-				buf.WriteString(row)
+				fmt.Print(fmt.Sprintf("%-16s%s\n", addr, host))
 			}
 		}
-	}
-	username := os.Getenv("username")
-	password := os.Getenv("password")
-	err := ioutil.WriteFile("hosts", buf.Bytes(), 0664)
-	checkErr(err)
-	err = exec.Command("git", "config", "user.name", username).Run()
-	checkErr(err)
-	err = exec.Command("git", "config", "user.email", "openset.wang@gmail.com").Run()
-	checkErr(err)
-	err = exec.Command("git", "config", "remote.origin.url", fmt.Sprintf("https://%s:%s@github.com/openset/hosts.git", username, password)).Run()
-	checkErr(err)
-	err = exec.Command("git", "commit", "-am", "weekly update").Run()
-	checkErr(err)
-	err = exec.Command("git", "push", "origin", "master").Run()
-	checkErr(err)
-}
-
-func checkErr(err error) {
-	if err != nil {
-		log.Fatalln(err)
 	}
 }
